c45dbee6e2ac9bade7f8c34ec1367e53c7ff2036
Bia Oliveira <pellegrini.beatriz@gmail.com>
2023-04-26 09:01:35 -0700

    added category buttons & tested API

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,5 +1,38 @@
+import Button from './Button';
+import TestAPI from '../utilities/api';
+
 const Buttons = () => {
-  return "hello Buttons";
+	const categories = [
+		{
+			id: 'popular',
+			categoryName: 'Popular',
+		},
+		{
+			id: 'top-rated',
+			categoryName: 'Top Rated',
+		},
+		{
+			id: 'now-playing',
+			categoryName: 'Now Playing',
+		},
+		{
+			id: 'upcoming',
+			categoryName: 'Upcoming',
+		},
+	];
+
+	return (
+		<section className="category-buttons-section">
+			<ul>
+				{categories.map(category => (
+					<li key={category.id}>
+						<Button id={category.id} categoryName={category.categoryName} />
+					</li>
+				))}
+			</ul>
+			<TestAPI />
+		</section>
+	);
 };
 
 export default Buttons;
