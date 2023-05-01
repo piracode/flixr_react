4d7e5f1b5330e1d6248c007ed7f2232b05f604b4
shucheng56 <shucheng56@gmail.com>
2023-04-30 22:42:30 -0700

    update for hero.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,32 +1,45 @@
-import { useState, useEffect } from 'react';
-import useFetch from './../utilities/toolbelt';
-import { API_KEY } from './../utilities/api';
-import Movie from './Movie';
+import { useState, useEffect } from "react";
+import useFetch from "./../utilities/toolbelt";
+import { API_KEY } from "./../utilities/api";
+import Movie from "./Movie";
 
 const Movies = ({ chosenCategory }) => {
-	const { get } = useFetch('https://api.themoviedb.org/3/movie/');
-	const [movies, setMovies] = useState([]);
+  const { get } = useFetch("https://api.themoviedb.org/3/movie/");
+  const [movies, setMovies] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
-	useEffect(() => {
-		get(`${chosenCategory}?api_key=${API_KEY}&language=en-US&page=1`)
-			.then(data => {
-				setMovies(data.results);
-				console.log(data.results);
-			})
-			.catch(error => console.log(error)); /////// what do i do with this error? should i display it? ////////
-	}, [chosenCategory]);
+  const handleAddToFavorites = (movie) => {
+    setFavorites((prevFavorites) => [...prevFavorites, movie]);
+  };
 
-	if (movies) {
-		return (
-			<section className="movies-container">
-				{movies.map(movie => {
-					return <Movie key={movie.id} poster_path={movie.poster_path} title={movie.title} />;
-				})}
-			</section>
-		);
-	} else {
-		return <p className="error-message">Error: Unable to retrieve movies.</p>;
-	}
+  useEffect(() => {
+    get(`${chosenCategory}?api_key=${API_KEY}&language=en-US&page=1`)
+      .then((data) => {
+        setMovies(data.results);
+        console.log(data.results);
+      })
+      .catch((error) => console.log(error));
+  }, [chosenCategory]);
+
+  if (movies) {
+    return (
+      <section className="movies-container">
+        {movies.map((movie) => {
+          return (
+            <Movie
+              id={movie.id}
+              key={movie.id}
+              poster_path={movie.poster_path}
+              title={movie.title}
+              onAddToFavorites={() => handleAddToFavorites(movie)}
+            />
+          );
+        })}
+      </section>
+    );
+  } else {
+    return <p className="error-message">Error: Unable to retrieve movies.</p>;
+  }
 };
 
 export default Movies;
