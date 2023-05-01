a6165209ce97a13fc3ed915bef1baae03aeca3bf
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-05-01 13:48:51 -0700

    synced buttons on Navbar with buttons on Home  + implemented search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,39 +1,19 @@
 import { useState } from "react";
-
-const Search = () => {
-  const [inputValue, setInputValue] = useState("");
+import { useNavigate } from "react-router-dom";
+//
+const Search = ({ onSearchSubmit }) => {
+  const [searchInputValue, setSearchInputValue] = useState("");
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e.target.value);
-    console.log("Submitted value:", inputValue);
+    const searchValue = e.target.elements["search-input"].value;
+    setSearchInputValue(searchValue);
+    onSearchSubmit(searchValue);
+    navigate(`/searchResults?query=${searchValue}`);
+
+    e.target.reset();
   }
-
-  // function handleKeyDown(e) {
-  //   if (e.key === "Enter") {
-  //     e.preventDefault();
-  //     console.log("This is enter key");
-  //     handleSubmit(e);
-  //     // setInputValue("hi");
-  //   }
-  // }
-
-  // function handleClick(e) {
-  //   handleSubmit(e);
-  // }
-
-  // function handleChange(e) {
-  //   setInputValue(e.target.value);
-  //   console.log(e.target.className);
-
-  // if (e.target.className === "search__input") {
-  //   console.log("classname is search ionput");
-  // } else if (e.target.className === "search__svg") {
-  //   console.log("classname is svg");
-  // }
-
-  // console.log(inputValue);
-  // }
 
   return (
     <>
@@ -46,19 +26,16 @@ const Search = () => {
         <label htmlFor="search-input" className="visually-hidden search__label">
           Search for a movie:
         </label>
-        <input
-          type="text"
-          id="search-input"
-          name="search-input"
-          className="search__input"
-          placeholder="Movie title..."
-          // value={inputValue}
-          // onChange={handleChange}
-          // onClick={handleChange}
-          // onKeyDown={handleKeyDown}
-          // value={inputValue}
-        />
-        <button type="submit" className="search__svg"></button>
+        <div className="search__container">
+          <input
+            type="text"
+            id="search-input"
+            name="search-input"
+            className="search__input"
+            placeholder="Movie title..."
+          />
+          <button type="submit" className="search__svg"></button>
+        </div>
       </form>
     </>
   );
