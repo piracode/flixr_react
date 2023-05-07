df3371a3abc0993300384f640930f63b6e67e3c6
shucheng56 <shucheng56@gmail.com>
2023-05-07 10:39:16 -0700

    update the movie and favourites file

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,22 +1,46 @@
 import React, { useState } from "react";
+import star from "../assets/star.svg";
+import starToggle from "../assets/star-toggle.svg";
 
-const Movie = ({ id, poster_path, title }) => {
-  const [isStarred, setIsStarred] = useState(false);
+const Movie = ({
+  id,
+  poster_path,
+  title,
+  isFavouritePage,
+  isStarred,
+  onToggleStar,
+}) => {
+  const [isStarredState, setIsStarredState] = useState(isStarred);
 
   const toggleStar = () => {
-    setIsStarred(!isStarred);
-    if (!isStarred) {
-      // add movie to favorites if it's not already in favorites
-      const favoriteMovie = { id, poster_path, title };
-      const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
-      const updatedFavorites = storedFavorites
-        ? [...storedFavorites, favoriteMovie]
-        : [favoriteMovie];
-      localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    const updatedIsStarred = !isStarredState;
+    setIsStarredState(updatedIsStarred);
+
+    if (isFavouritePage) {
+      // Remove movie from favorites if the star is toggled off on the favorites page
+      onToggleStar();
+    } else {
+      if (!updatedIsStarred) {
+        // Remove movie from favorites if it's already in favorites
+        const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
+        const updatedFavorites = storedFavorites.filter(
+          (movie) => movie.id !== id
+        );
+        localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+      } else {
+        // Add movie to favorites if it's not already in favorites
+        const favoriteMovie = { id, poster_path, title };
+        const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
+        const updatedFavorites = storedFavorites
+          ? [...storedFavorites, favoriteMovie]
+          : [favoriteMovie];
+        localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+      }
     }
   };
 
-  const starClass = isStarred ? "star-toggle" : "star-image";
+  const starImage = isStarredState ? starToggle : star;
+  const starClass = isStarredState ? "star-toggle" : "star-image";
 
   return (
     <article className="individual-movie-container">
@@ -24,36 +48,14 @@ const Movie = ({ id, poster_path, title }) => {
         key={id}
         src={`https://image.tmdb.org/t/p/w300${poster_path}`}
         alt={title}
-        className="image-wrapper"
+        className="image-container"
       />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 800 800"
+      <img
+        src={starImage}
+        alt="star"
         className={starClass}
         onClick={toggleStar}
-      >
-        <style>
-          {`.star-image,
-            .star-toggle {
-              fill: transparent;
-              stroke: #8E8EED;
-              stroke-width: 50;
-              stroke-linecap: round;
-              stroke-linejoin: round;
-              stroke-miterlimit: 133.3333;
-            }
-            .star-toggle .st0 {
-              fill: #8E8EED;
-            }`}
-        </style>
-        <path
-          className="st0"
-          d="M457.7,117l58.7,117.3c8,16.3,29.3,32,47.3,35L670,287c68,11.3,84,60.7,35,109.3L622.3,479
-          c-14,14-21.7,41-17.3,60.3l23.7,102.3c18.7,81-24.3,112.3-96,70l-99.7-59c-18-10.7-47.7-10.7-66,0l-99.7,59
-          c-71.3,42.3-114.7,10.7-96-70L195,539.3c4.3-19.3-3.3-46.3-17.3-60.3L95,396.3c-48.7-48.7-33-98,35-109.3l106.3-17.7
-          c17.7-3,39-18.7,47-35L342,117C374,53.3,426,53.3,457.7,117z"
-        />
-      </svg>
+      />
     </article>
   );
 };
