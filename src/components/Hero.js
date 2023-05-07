e95bf398d14febbe081673e2b9f1be61314c35db
Martha Villa Martin <34667426+piracode@users.noreply.github.com>
2023-05-07 10:26:04 -0700

    added question mark svg, added text and button in hero, ad try/catch in api

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,27 @@
-import { useState, useEffect } from "react";
-import useFetch from "./../utilities/toolbelt";
-import { API_KEY } from "./../utilities/api";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { useState, useEffect } from 'react'
+import useFetch from './../utilities/toolbelt'
+import { API_KEY } from './../utilities/api'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+// import question from './../assets/question-mark.svg'
 
 const Hero = () => {
-  const { get } = useFetch("https://api.themoviedb.org/3/movie/");
-  const [movies, setMovies] = useState([]);
-  const [activeSlide, setActiveSlide] = useState(0);
+  const { get } = useFetch('https://api.themoviedb.org/3/movie/')
+  const [movies, setMovies] = useState([])
+  // const [activeSlide, setActiveSlide] = useState(0)
 
   useEffect(() => {
     get(`popular?api_key=${API_KEY}&language=en-US&page=1`)
       .then((data) => {
-        setMovies(data.results);
+        setMovies(data.results)
+        console.log(data.results)
       })
-      .catch((error) => console.log(error));
-  }, []);
+      .catch((error) => console.log(error))
+  }, [])
+  // })
 
-  const posterPaths = movies.map((movie) => movie.backdrop_path);
+  const posterPaths = movies.map((movie) => movie.backdrop_path)
 
   const sliderSettings = {
     dots: true,
@@ -28,51 +31,62 @@ const Hero = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
-    beforeChange: (current, next) => setActiveSlide(next),
-    initialSlide: activeSlide,
-  };
+    // beforeChange: (current, next) => setActiveSlide(next),
+    // initialSlide: activeSlide,
+  }
 
   return (
-    <section className="hero">
-      <Slider {...sliderSettings}>
-        <div className="hero-image-container">
-          <img
-            src={`https://image.tmdb.org/t/p/w1280/${posterPaths[0]}`}
-            alt="Movie Poster 1"
-            className="hero-image"
-          />
-        </div>
-        <div className="hero-image-container">
-          <img
-            src={`https://image.tmdb.org/t/p/w1280/${posterPaths[1]}`}
-            alt="Movie Poster 2"
-            className="hero-image"
-          />
-        </div>
-        <div className="hero-image-container">
-          <img
-            src={`https://image.tmdb.org/t/p/w1280/${posterPaths[2]}`}
-            alt="Movie Poster 3"
-            className="hero-image"
-          />
-        </div>
-      </Slider>
-      <div className="slider-buttons">
-        <button
-          className={`slider-button ${activeSlide === 0 ? "active" : ""}`}
-          onClick={() => setActiveSlide(0)}
-        ></button>
-        <button
-          className={`slider-button ${activeSlide === 1 ? "active" : ""}`}
-          onClick={() => setActiveSlide(1)}
-        ></button>
-        <button
-          className={`slider-button ${activeSlide === 2 ? "active" : ""}`}
-          onClick={() => setActiveSlide(2)}
-        ></button>
-      </div>
-    </section>
-  );
-};
+    <section className='hero'>
+      <section className='hero'>
+        <Slider {...sliderSettings}>
+          {/* the c=slice only returns the first 3 items of the array so it is not random but I beliebe the image of the poster change over time as new movies come? 
+          
+          GPT also gives me a solution whcih coms from the lodash library and a way to return random index cfrom the movie array. I copied code on the bottom*/}
+          {posterPaths.slice(0, 3).map((path, index) => {
+            const movie = movies[index]
+            const truncatedText = movie.overview.slice(0, 140) + '...'
+            return (
+              <div key={`poster-${index}`} className='hero-image-container'>
+                <div className='hero-info'>
+                  <h2 className='hero-info-movie-title'>{movie.title}</h2>
+                  <p className='hero-info-movie-description'>{truncatedText}</p>
+                  <div className='hero-info-button-container'>
+                    <button className='hero-info-button'>Learn More</button>
+                  </div>
+                </div>
+                <img
+                  src={`https://image.tmdb.org/t/p/w1280/${path}`}
+                  alt={`Movie Poster ${index + 1}`}
+                  className='hero-image'
+                />
+              </div>
+            )
+          })}
+        </Slider>
+      </section>
 
-export default Hero;
+      {/* import _ from 'lodash';
+
+const randomMovies = _.sampleSize(movies, 3);
+
+<Slider {...sliderSettings}>
+  {randomMovies.map((movie) => (
+    <div key={movie.id} className='hero-image-container'>
+      <div className='hero-info'>
+        <h2 className='hero-movie-title'>{movie.title}</h2>
+        <p className='hero-movie-description'>{movie.overview}</p>
+      </div>
+      <img
+        src={`https://image.tmdb.org/t/p/w1280/${movie.poster_path}`}
+        alt={movie.title}
+        className='hero-image'
+      />
+    </div>
+  ))}
+</Slider>
+ */}
+    </section>
+  )
+}
+
+export default Hero
