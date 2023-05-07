a106281a16b4ed27fab6262affeede2730d8543d
Bia Oliveira <pellegrini.beatriz@gmail.com>
2023-05-07 13:51:36 -0700

    added styles for movie hover, favourites page, and some additional fixes

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,41 +1,40 @@
-import { useState, useEffect } from "react";
-import useFetch from "./../utilities/toolbelt";
-import { API_KEY } from "./../utilities/api";
-import Movie from "../components/Movie";
+import { useState, useEffect } from 'react';
+import useFetch from './../utilities/toolbelt';
+import { API_KEY } from './../utilities/api';
+import Movie from '../components/Movie';
 
 const Movies = ({ chosenCategory }) => {
-  const { get } = useFetch("https://api.themoviedb.org/3/movie/");
-  const [movies, setMovies] = useState([]);
+	const { get } = useFetch('https://api.themoviedb.org/3/movie/');
+	const [movies, setMovies] = useState([]);
 
-  useEffect(() => {
-    get(`${chosenCategory}?api_key=${API_KEY}&language=en-US&page=1`)
-      .then((data) => {
-        setMovies(data.results);
-        console.log(data.results);
-      })
-      .catch((error) => console.log(error)); /////// what do i do with this error? should i display it? ////////
-  }, [chosenCategory]);
+	useEffect(() => {
+		get(`${chosenCategory}?api_key=${API_KEY}&language=en-US&page=1`)
+			.then(data => {
+				setMovies(data.results);
+				console.log(data.results);
+			})
+			.catch(error => console.log(error)); /////// what do i do with this error? should i display it? ////////
+	}, [chosenCategory]);
 
-  if (movies) {
-    return (
-      <section className="movies-container">
-        {movies.map((movie) => {
-          return (
-            <>
-              <Movie
-                key={movie.id}
-                poster_path={movie.poster_path}
-                title={movie.title}
-                id={movie.id}
-              />
-            </>
-          );
-        })}
-      </section>
-    );
-  } else {
-    return <p className="error-message">Error: Unable to retrieve movies.</p>;
-  }
+	if (movies) {
+		return (
+			<section className="movies-container">
+				{movies.map(movie => {
+					return (
+						<div className="individual-movie">
+							<Movie
+								key={movie.id}
+								className="individual-movie-container"
+								id={movie.id}
+							/>
+						</div>
+					);
+				})}
+			</section>
+		);
+	} else {
+		return <p className="error-message">Error: Unable to retrieve movies.</p>;
+	}
 };
 
 export default Movies;
--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,39 +1,44 @@
-import React, { useState, useEffect } from "react";
-import Movie from "../components/Movie";
+import React, { useState, useEffect } from 'react';
+import Movie from '../components/Movie';
 
 const Favourites = () => {
-  const [favorites, setFavorites] = useState([]);
+	const [favorites, setFavorites] = useState([]);
 
-  useEffect(() => {
-    const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
-    if (storedFavorites) {
-      setFavorites(storedFavorites);
-    }
-  }, []);
+	useEffect(() => {
+		const storedFavorites = JSON.parse(localStorage.getItem('favorites'));
+		if (storedFavorites) {
+			setFavorites(storedFavorites);
+		}
+	}, []);
 
-  const handleRemoveFavorite = (id) => {
-    const updatedFavorites = favorites.filter((movie) => movie.id !== id);
-    setFavorites(updatedFavorites);
-    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
-  };
+	const handleRemoveFavorite = id => {
+		const updatedFavorites = favorites.filter(movie => movie.id !== id);
+		setFavorites(updatedFavorites);
+		localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+	};
 
-  return (
-    <div className="favourites-container">
-      <h1>Favourites</h1>
-      {favorites.length === 0 && <p>You have no favorite movies yet.</p>}
-      {favorites.map((movie) => (
-        <Movie
-          key={movie.id}
-          id={movie.id}
-          poster_path={movie.poster_path}
-          title={movie.title}
-          isStarred={true} // Pass the isStarred prop as true
-          onToggleStar={() => handleRemoveFavorite(movie.id)} // Pass a function to remove the movie from favourites
-          isFavouritePage={true}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<section className="favourites-container">
+			<h2>Favourites</h2>
+			<div className="movies-container">
+				{favorites.length === 0 && (
+					<div className="no-favourites">
+						<p>You have no favourite movies yet.</p>
+					</div>
+				)}
+				{favorites.map(movie => (
+					<Movie
+						key={movie.id}
+						id={movie.id}
+						isStarred={true} // Pass the isStarred prop as true
+						onToggleStar={() => handleRemoveFavorite(movie.id)} // Pass a function to remove the movie from favourites
+						isFavouritePage={true}
+						className="individual-movie-container"
+					/>
+				))}
+			</div>
+		</section>
+	);
 };
 
 export default Favourites;
