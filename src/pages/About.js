e53ab76b853192b0614d6204d2ede3b908983b7e
Bia Oliveira <pellegrini.beatriz@gmail.com>
2023-05-07 10:37:04 -0700

    added about, footer, error imgs, button styles, hover styles, etc

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,23 @@
+import flixrCamera from './../assets/camera.svg';
+
 const About = () => {
-  return "hello about";
+	return (
+		<section className="about-page">
+			<h2>flixr</h2>
+			<img src={flixrCamera} alt="camera"></img>
+			<h3>Your Ultimate Movie Companion</h3>
+			<p>
+				At Flixr, we are passionate about movies and strive to bring you the
+				best movie experience possible. Our team of movie enthusiasts has
+				developed a movie database app that is both comprehensive and
+				user-friendly.
+			</p>
+			<p>
+				With Flixr, you can discover new movies, stay up to date on the latest
+				releases, and find the perfect flick to watch on any given day.
+			</p>
+		</section>
+	);
 };
 
 export default About;
