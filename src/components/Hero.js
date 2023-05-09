7a2923f50d472654657eeaed1436f2bb4f66e4ef
shucheng56 <shucheng56@gmail.com>
2023-05-08 17:39:47 -0700

    merge from main

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react'
-import useFetch from './../utilities/toolbelt'
-import { API_KEY } from './../utilities/api'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+import { useState, useEffect } from "react";
+import useFetch from "./../utilities/toolbelt";
+import { API_KEY } from "./../utilities/api";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Hero = () => {
-  const { get } = useFetch('https://api.themoviedb.org/3/movie/')
-  const [movies, setMovies] = useState([])
+  const { get } = useFetch("https://api.themoviedb.org/3/movie/");
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     get(`popular?api_key=${API_KEY}&language=en-US&page=1`)
       .then((data) => {
-        setMovies(data.results)
-        console.log(data.results)
+        setMovies(data.results);
+        console.log(data.results);
       })
-      .catch((error) => console.log(error))
-  }, [])
+      .catch((error) => console.log(error));
+  }, []);
   // })
 
-  const posterPaths = movies.map((movie) => movie.backdrop_path)
+  const posterPaths = movies.map((movie) => movie.backdrop_path);
 
   const sliderSettings = {
     dots: true,
@@ -30,34 +30,34 @@ const Hero = () => {
     autoplay: true,
     autoplaySpeed: 5000,
     arrows: false,
-  }
+  };
 
   return (
-    <section className='hero'>
+    <section className="hero">
       <Slider {...sliderSettings}>
         {posterPaths.slice(0, 3).map((path, index) => {
-          const movie = movies[index]
-          const truncatedText = movie.overview.slice(0, 110) + '...'
+          const movie = movies[index];
+          const truncatedText = movie.overview.slice(0, 110) + "...";
           return (
-            <div key={`poster-${index}`} className='hero-image-container'>
-              <div className='hero-info'>
-                <h2 className='hero-info-movie-title'>{movie.title}</h2>
-                <p className='hero-info-movie-description'>{truncatedText}</p>
-                <div className='hero-info-button-container'>
-                  <button className='hero-info-button'>Learn More</button>
+            <div key={`poster-${index}`} className="hero-image-container">
+              <div className="hero-info">
+                <h2 className="hero-info-movie-title">{movie.title}</h2>
+                <p className="hero-info-movie-description">{truncatedText}</p>
+                <div className="hero-info-button-container">
+                  <button className="hero-info-button">Learn More</button>
                 </div>
               </div>
               <img
                 src={`https://image.tmdb.org/t/p/w1280/${path}`}
                 alt={`Movie Poster ${index + 1}`}
-                className='hero-image'
+                className="hero-image"
               />
             </div>
-          )
+          );
         })}
       </Slider>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -1,44 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import Movie from '../components/Movie';
+import React, { useState, useEffect } from "react";
+import Movie from "../components/Movie";
+import star from "../assets/star.svg";
 
 const Favourites = () => {
-	const [favorites, setFavorites] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
-	useEffect(() => {
-		const storedFavorites = JSON.parse(localStorage.getItem('favorites'));
-		if (storedFavorites) {
-			setFavorites(storedFavorites);
-		}
-	}, []);
+  useEffect(() => {
+    const storedFavorites = JSON.parse(localStorage.getItem("favorites"));
+    if (storedFavorites) {
+      setFavorites(storedFavorites);
+    }
+  }, []);
 
-	const handleRemoveFavorite = id => {
-		const updatedFavorites = favorites.filter(movie => movie.id !== id);
-		setFavorites(updatedFavorites);
-		localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-	};
+  const handleRemoveFavorite = (id) => {
+    const updatedFavorites = favorites.filter((movie) => movie.id !== id);
+    setFavorites(updatedFavorites);
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+  };
 
-	return (
-		<section className="favourites-container">
-			<h2>Favourites</h2>
-			<div className="movies-container">
-				{favorites.length === 0 && (
-					<div className="no-favourites">
-						<p>You have no favourite movies yet.</p>
-					</div>
-				)}
-				{favorites.map(movie => (
-					<Movie
-						key={movie.id}
-						id={movie.id}
-						isStarred={true} // Pass the isStarred prop as true
-						onToggleStar={() => handleRemoveFavorite(movie.id)} // Pass a function to remove the movie from favourites
-						isFavouritePage={true}
-						className="individual-movie-container"
-					/>
-				))}
-			</div>
-		</section>
-	);
+  return (
+    <section className="favourites-container">
+      <h2>Favourites</h2>
+      <div className="movies-container">
+        {favorites.length === 0 && (
+          <div className="no-favourites">
+            <p>
+              You have no favourite movies yet. Add favourite movies by clicking
+              the <img className="fav-star" src={star} alt="star" /> on the
+              movie cards.
+            </p>
+          </div>
+        )}
+        {favorites.map((movie) => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            isStarred={true} // Pass the isStarred prop as true
+            onToggleStar={() => handleRemoveFavorite(movie.id)} // Pass a function to remove the movie from favourites
+            isFavouritePage={true}
+            className="individual-movie-container"
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Favourites;
